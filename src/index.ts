c64634035fd54c6a4019e11194c7340ddfeb5518
Tsetsko <tseko0301@gmail.com>
2025-09-02 13:05:19 +0800

    url nemsen

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,23 @@ import playerRouters from "./routes/participant-routes";
 const app = express();
 const PORT = 4200;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://lucky-bites.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
